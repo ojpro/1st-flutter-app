a6994905323e24d06519de75853e1121492643f0
ojpro <contact@ojpro.me>
2023-12-24 12:44:59 +0100

    feat(HomeScreen): re-design the home layout

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,7 +14,7 @@ class HomeScreen extends StatelessWidget {
           ),
           actions: [
             Padding(
-              padding: const EdgeInsets.only(right: 8.0),
+              padding: const EdgeInsetsDirectional.only(end: 8.0),
               child: IconButton(
                   icon: const Icon(Icons.search, color: Colors.blueAccent),
                   onPressed: () {
@@ -25,89 +25,80 @@ class HomeScreen extends StatelessWidget {
         ),
         body: SafeArea(
           child: Container(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+            height: double.infinity,
+            color: Colors.black,
             child: SingleChildScrollView(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.grey,
-                    height: 200,
-                    child: const Text('Mercury', style: TextStyle(color: Colors.white),),
-                  ),
+                      padding: const EdgeInsets.all(8),
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.2),
+                          borderRadius: BorderRadius.circular(4.0)),
+                      height: 200,
+                      child: const Text(
+                        'Mercury',
+                        style: TextStyle(color: Colors.white),
+                      )),
                   const SizedBox(
-                    height: 12,
+                    height: 6,
                   ),
                   Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.orange,
-                    height: 200,
-                    child: const Text('Venus', style: TextStyle(color: Colors.white),),
-                  ),
+                      padding: const EdgeInsets.all(8),
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.2),
+                          borderRadius: BorderRadius.circular(4.0)),
+                      height: 200,
+                      child: const Text(
+                        'Earth',
+                        style: TextStyle(color: Colors.white),
+                      )),
                   const SizedBox(
-                    height: 12,
+                    height: 6,
                   ),
                   Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.blue,
-                    height: 200,
-                    child: const Text('Earth', style: TextStyle(color: Colors.white),),
-                  ),
+                      padding: const EdgeInsets.all(8),
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.2),
+                          borderRadius: BorderRadius.circular(4.0)),
+                      height: 200,
+                      child: const Text(
+                        'Mars',
+                        style: TextStyle(color: Colors.white),
+                      )),
                   const SizedBox(
-                    height: 12,
+                    height: 6,
                   ),
                   Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.red,
-                    height: 200,
-                    child: const Text('Mars', style: TextStyle(color: Colors.white),),
-                  ),
+                      padding: const EdgeInsets.all(8),
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.2),
+                          borderRadius: BorderRadius.circular(4.0)),
+                      height: 200,
+                      child: const Text(
+                        'Uranus',
+                        style: TextStyle(color: Colors.white),
+                      )),
                   const SizedBox(
-                    height: 12,
+                    height: 6,
                   ),
                   Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.brown,
-                    height: 200,
-                    child: const Text('Jupiter', style: TextStyle(color: Colors.white),),
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.amber,
-                    height: 200,
-                    child: const Text('Saturn', style: TextStyle(color: Colors.white),),
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.cyan,
-                    height: 200,
-                    child: const Text('Uranus', style: TextStyle(color: Colors.white),),
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.blueAccent,
-                    height: 200,
-                    child: const Text('Neptune', style: TextStyle(color: Colors.white),),
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.black12,
-                    height: 200,
-                    child: const Text('Pluto', style: TextStyle(color: Colors.white),),
-                  ),
+                      padding: const EdgeInsets.all(8),
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.2),
+                          borderRadius: BorderRadius.circular(4.0)),
+                      height: 200,
+                      child: const Text(
+                        'Neptune',
+                        style: TextStyle(color: Colors.white),
+                      )),
                 ],
               ),
             ),
