987d5b2cf30b248cfd658b033c6a4f7ef98bce77
ojpro <contact@ojpro.me>
2023-12-24 15:08:48 +0100

    feat(LoginScreen): created a simple Login screen UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/screens/auth/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,8 +11,9 @@ class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text("Hello World"),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
     );
   }
 
